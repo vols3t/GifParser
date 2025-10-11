c26b92daa014bbb48414d5d20461a637f95c3a2b
Stepa_Volkov <wdgetso2@gmail.com>
2025-10-11 21:18:57 +0500

    first vers pepapig-sigma

---
+++ parser.py
@@ -0,0 +1,10 @@
+
+with open("pepapig-sigma.gif","rb") as f:
+    data = f.read()
+    f.close()
+
+header = data[:6].decode("ascii")
+wh = int.from_bytes(data[6:8], "little")
+ht = int.from_bytes(data[8:10], "little")
+
+print(header, wh, ht)
